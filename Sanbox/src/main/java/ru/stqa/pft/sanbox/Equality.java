64822146a6cdfeb7ca5d01c15b54615a41169f9d
almat1797 <almat197@gmail.com>
2021-01-15 14:49:14 +0600

    Создал новый класс для примера сравнения Equality

---
+++ Sanbox/src/main/java/ru/stqa/pft/sanbox/Equality.java
@@ -0,0 +1,14 @@
+package ru.stqa.pft.sanbox;
+
+public class Equality {
+
+  public static void main(String[] args) {
+    String s1 = "Firefox 2.0";
+    String s2 = "Firefox" + Math.sqrt(4.0);
+
+
+    System.out.println(s1 == s2);
+    System.out.println(s1.equals(s2));
+
+  }
+}
